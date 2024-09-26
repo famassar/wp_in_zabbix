d181707da2bd6b286f1709cf895177d296101278
Fabrizio Massari <79312342+famassar@users.noreply.github.com>
2024-09-27 01:26:11 +0200

    Update discover_files.py
    Updated release for python3

--- discover_files.py
+++ discover_files.py
@@ -1,39 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import os,json,sys,re
-from base64 import b64decode
+import os
+import json
+import sys
+import re
+import base64
 
 data = []
 
-if len(sys.argv)<3:
-  print json.dumps({'data':data,'error':'Usage: discover_files.py path b64_fname_regex'})
-  sys.exit(1)
+if len(sys.argv) < 3:
+    print(json.dumps({'data': data, 'error': 'Usage: discover_files.py path b64_fname_regex'}))
+    sys.exit(1)
 
 if not os.path.isdir(sys.argv[1]):
-  print json.dumps({'data':data,'error': sys.argv[1]+' is not a directory'})
-  sys.exit(1)
+    print(json.dumps({'data': data, 'error': sys.argv[1] + ' is not a directory'}))
+    sys.exit(1)
 
 def find_all(name, path):
-  result = []
-  # try to decode name
-  try:
-    name=b64decode(name)
-  except Exception as e:
-    print json.dumps({'data':data,'error': 'Can\'t decode '+name+' as base64. '+str(e)})
-    sys.exit(1)
-  # try regex
-  try:
-    reobj = re.compile(name)
-  except Exception as e:
-    print json.dumps({'data':data,'error': 'Can\'t compile '+name+' as regex. '+str(e)})
-    sys.exit(1)
-  for root, dirs, files in os.walk(path):
-    for fname in files:
-      if reobj.search(fname):
-        result.append(os.path.join(root, fname))
-  return result
+    result = []
+    try:
+        name = base64.b64decode(name).decode('utf-8')
+    except Exception as e:
+        print(json.dumps({'data': data, 'error': 'Can\'t decode ' + name + ' as base64. ' + str(e)}))
+        sys.exit(1)
+    try:
+        reobj = re.compile(name)
+    except Exception as e:
+        print(json.dumps({'data': data, 'error': 'Can\'t compile ' + name + ' as regex. ' + str(e)}))
+        sys.exit(1)
+    for root, dirs, files in os.walk(path):
+        for fname in files:
+            if reobj.search(fname):
+                result.append(os.path.join(root, fname))
+    return result
 
-for fpath in find_all(sys.argv[2],sys.argv[1]):
-  data.append({'{#FDIR}':os.path.dirname(fpath),'{#FDIRNAME}':os.path.basename(os.path.dirname(fpath)),'{#FPATH}':fpath,'{#FNAME}':os.path.basename(fpath)})
+for fpath in find_all(sys.argv[2], sys.argv[1]):
+    data.append({'{#FDIR}': os.path.dirname(fpath), '{#FDIRNAME}': os.path.basename(os.path.dirname(fpath)), '{#FPATH}': fpath, '{#FNAME}': os.path.basename(fpath)})
 
-print json.dumps({'data':data})
+print(json.dumps({'data': data}))
